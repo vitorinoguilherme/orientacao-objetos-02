369b86e9d59342991d45a59d012a153d7452c732
Guilherme Vitorino <vitorino.guilherme@users.noreply.github.com>
2020-03-07 20:19:24 -0300

    Criado classe Circulo

---
+++ src/main/java/exercicios/Circulo.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+public class Circulo {
+    private Double raio;
+
+    public Circulo(Double raio) {
+        if ( raio > 0 )
+            this.raio = raio;
+    }
+
+    public Double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    public Double calcularCircunferencia() {
+        return 2 * Math.PI * raio;
+    }
+
+    public void getRaio(Double raio) throws IllegalAccessException {
+        if ( raio <= 0 )
+            throw new IllegalArgumentException("Valor deve ser maior que zero.");
+        this.raio = raio;
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+}
